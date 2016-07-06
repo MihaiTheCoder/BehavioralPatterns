d5d4fc3f86fcd1e58fc369763f3e0195da015cf6
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2016-07-06 11:54:25 +0300

    Add VisitorPattern example

---
+++ src/VisitorPattern/VisitorPatternExamples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class VisitorPatternExamples
+    {
+        public VisitorPatternExamples()
+        {
+        }
+
+        public static void Run()
+        {
+
+        }
+
+        public static string GetDescription()
+        {
+            return @"Visitor pattern allows for one or more operation to be applied to a set of objects at runtime, decoupling the operations from the object structure. ";
+        }
+    }
+}
